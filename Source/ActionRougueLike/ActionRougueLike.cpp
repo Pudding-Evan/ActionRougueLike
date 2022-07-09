714b2a3c5ddf1779f5e54863f5107a06ee485c7a
Pudding-Evan <40421391+Pudding-Evan@users.noreply.github.com>
2022-07-09 15:20:11 +0800

    Added Asset & Setup Input
    - Add Character Asset
    - Added Movement forward and turn

---
+++ Source/ActionRougueLike/ActionRougueLike.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ActionRougueLike.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ActionRougueLike, "ActionRougueLike" );
---
+++ Source/ActionRougueLike/ActionRougueLikeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ActionRougueLikeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROUGUELIKE_API AActionRougueLikeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
